d3964e9e7da869af59f5e70c850fbf8061c6a8b2
pym96 <pym66@outlook.com>
2023-06-03 15:08:05 +1000

    Second commit

---
+++ build/include/CMakeFiles/auto_aim.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yiming/training/c++_further_no_cost/include/auto_aim.cpp" "/home/yiming/training/c++_further_no_cost/build/include/CMakeFiles/auto_aim.dir/auto_aim.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../auto_aim"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/include/CMakeFiles/auto_aim.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auto_aim.dir/auto_aim.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/auto_aim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
